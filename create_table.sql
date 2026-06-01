f26e22e1294759b7c1ef8e58d91238ec886b242e
KArunKumar <arunkkarukolla@gmail.com>
2026-06-01 23:34:39 +0530

    Add create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE EmployeeAttrition (
+    EmployeeID INT PRIMARY KEY,
+    Age INT,
+    Department VARCHAR(50),
+    JobRole VARCHAR(100),
+    MonthlyIncome INT,
+    YearsAtCompany INT,
+    Attrition VARCHAR(5)
+);
